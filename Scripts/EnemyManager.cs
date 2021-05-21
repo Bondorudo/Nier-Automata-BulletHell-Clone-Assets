55579c116e1bb90feffb1e80060a6c35b14a49a0
Bondorudo <thomas.aaltonen@hotmail.com>
2021-05-21 10:17:55 +0300

    Initial commit

---
+++ Scripts/EnemyManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private PlayerController thePlayer;
+    public GameObject enemy;
+    public GunController theGun;
+
+    public float speed = 3;
+    public float damping = 5;
+
+    public int health = 3;
+    private int currentHealth;
+    
+    public bool canShoot = false;
+
+    Vector3 offSet = new Vector3(0, 0, 1);
+
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        thePlayer = FindObjectOfType<PlayerController>();
+
+        //set current health to be equal to starting health
+        currentHealth = health;
+    }
+    
+    void Update()
+    {
+        //Enemy rotates to look at the player
+        var rotation = Quaternion.LookRotation(thePlayer.transform.position - enemy.transform.position);
+        enemy.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+
+
+        //Destroy enemy object when its health is 0
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+        //Enemy can fire their gun when canShoot is true
+        if (canShoot == true)
+        {
+            theGun.isFiring = true;
+        }
+    }
+
+    //Enemy movement
+    private void FixedUpdate()
+    {
+        enemyRb.velocity = (transform.forward * speed);
+    }
+
+    //Enemy takes damage
+    public void HurtEnemy(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
---
+++ Scripts/PlayerHealthManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    private Renderer rend;
+    public GameManager gameManager;
+
+    public float flashLength;
+    private float flashCounter;
+
+    public int startingHealth;
+    public int currentHealth;
+
+    private Color storedColor;
+    
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+        rend = GetComponent<Renderer>();
+        storedColor = rend.material.GetColor("_Color");
+    }
+    
+    void Update()
+    {
+        //Game over happens if player health is equal to 0
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+            gameManager.GameOver();
+        }
+        //Player color is restored to normal
+        if (flashCounter > 0)
+        {
+            flashCounter -= Time.deltaTime;
+            if (flashCounter <= 0)
+            {
+                rend.material.SetColor("_Color", storedColor);
+            }
+        }
+
+        gameManager.healthText.text = "Health: " + currentHealth;
+    }
+
+    //When player is hurt they lose health and their color turns red
+    public void HurtPlayer(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        flashCounter = flashLength;
+        rend.material.SetColor("_Color", Color.red);
+    }
+}
